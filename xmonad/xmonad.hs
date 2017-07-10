2844b926d23e8f43eee9ed4a1fc3672497d3f3cd
John Galbraith <john.galbraith17@gmail.com>
2017-07-09 23:58:54 -0700

    workable xmonad setup, at least on laptop

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,26 +1,29 @@
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog(xmobar)
 import XMonad.Util.Run(spawnPipe)
--- import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs" 
-    xmonad $ desktopConfig
+    spawnPipe "compton -b"
+    spawnPipe "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 26"
+    spawnPipe "hsetroot -solid '#000077'"
+    xmonad =<< xmobar desktopConfig
         { 
             terminal = "termite",
             modMask = mod4Mask,
             borderWidth = 2,
             layoutHook = avoidStruts $ 
                          smartSpacingWithEdge 5 $ 
-                         Tall 1 (3/100) (1/2) ||| Full,
-            manageHook = manageHook defaultConfig <+> manageDocks 
+                         Tall 1 (3/100) (1/2) ||| 
+                         Full,
+            manageHook = manageDocks <+> manageHook defaultConfig
             } 
-            `additionalKeys` [
+            -- `additionalKeys` [
                 -- ((mod4Mask .|. shiftMask, xK_g), sendMessage $ ToggleGaps)
-            ]
+            -- ]
 
 
