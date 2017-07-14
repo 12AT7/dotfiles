263376a7f19409e17a264bec8cb8fd60b2b7191e
John Galbraith <johng@alleninstitute.org>
2017-07-14 16:05:42 -0700

    Partial fix for XMonad pipe overflow lock bug

--- vimrc
+++ vimrc
@@ -1,5 +1,25 @@
+set nocompatible
+filetype off
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'lifepillar/vim-solarized8'
+Plugin 'flazz/vim-colorschemes'
+call vundle#end()
+filetype plugin indent on
+
 syntax on
 set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab
 
 set relativenumber
 set number
+
+colorscheme wombat
+
+fun! Solarized8Contrast(delta)
+  let l:schemes = map(["_low", "_flat", "", "_high"], '"solarized8_".(&background).v:val')
+  exe "colors" l:schemes[((a:delta+index(l:schemes, g:colors_name)) % 4 + 4) % 4]
+endf
+
+nmap <leader>- :<c-u>call Solarized8Contrast(-v:count1)<cr>
+nmap <leader>+ :<c-u>call Solarized8Contrast(+v:count1)<cr>
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,18 +1,20 @@
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.DynamicLog(xmobar)
+import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Layout.Spacing
-import XMonad.Layout.Accordion
+import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import qualified XMonad.StackSet as W
+import XMonad.Hooks.ManageHelpers
 
 main = do
-    -- spawnPipe "compton -b"
-    -- spawnPipe "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 26"
-    -- spawnPipe "hsetroot -solid '#000077'"
+    spawnPipe "compton -b"
+    spawnPipe "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 26"
+    spawnPipe "hsetroot -solid '#000077'"
     xmonad =<< xmobar desktopConfig
         { 
             terminal = "termite",
@@ -20,9 +22,11 @@ main = do
             borderWidth = 2,
             layoutHook = avoidStruts $ 
                          smartSpacingWithEdge 5 $ 
-                         Tall 1 (3/100) (1/2) ||| 
-                         Full ||| Accordion,
-            manageHook = manageDocks <+> manageHook defaultConfig
+                         Mirror (ThreeCol 1 (3/100) (1/2)) ||| Full,
+                         -- Mirror (Tall 1 (3/100) (1/2)) ||| Full,
+            -- manageHook = manageDocks <+> manageHook defaultConfig
+            manageHook = isDialog --> doF W.shiftMaster <+> doF W.focusDown,
+            logHook = dynamicLogWithPP $ xmobarPP
         } 
 
 
