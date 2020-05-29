d7ef4d2b310caedd80ad144f65fa34f512fa2a21
John Galbraith <john.galbraith@ariacoustics.com>
2020-05-28 19:52:48 -0700

    Aria laptop config

--- vimrc
+++ vimrc
@@ -1,29 +1,22 @@
 set nocompatible
+set noshowmode
 filetype off
-set rtp+=~/.vim/bundle/Vundle.vim
-call vundle#begin()
-Plugin 'VundleVim/Vundle.vim'
-Plugin 'lifepillar/vim-solarized8'
-Plugin 'flazz/vim-colorschemes'
-Plugin 'JamshedVesuna/vim-markdown-preview'
-call vundle#end()
 filetype plugin indent on
-
+set backspace=indent,eol,start
+let g:python_highlight_all = 1
+let g:better_whitespace_enabled = 1
+let g:strip_whitespace_on_save = 1
+let g:strip_whitespace_confirm = 0
+let g:strip_whitelines_at_eof = 1
+let g:airline_powerline_fonts = 1
 syntax on
-set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab
+nmap =j :%!python -m json.tool
 
-set relativenumber
-set number
+set undofile " Maintain undo history between sessions
+set undodir=~/.vim/undodir
 
-colorscheme wombat
-
-fun! Solarized8Contrast(delta)
-  let l:schemes = map(["_low", "_flat", "", "_high"], '"solarized8_".(&background).v:val')
-  exe "colors" l:schemes[((a:delta+index(l:schemes, g:colors_name)) % 4 + 4) % 4]
-endf
-
-nmap <leader>- :<c-u>call Solarized8Contrast(-v:count1)<cr>
-nmap <leader>+ :<c-u>call Solarized8Contrast(+v:count1)<cr>
-
-let vim_markdown_preview_github=1
-let vim_markdown_preview_browser='Chromium'
+autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4
+autocmd Filetype go setlocal tabstop=4 shiftwidth=4 softtabstop=4
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,36 +7,48 @@ import XMonad.Util.SpawnOnce
 import XMonad.Layout
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
+import XMonad.Actions.WorkspaceNames
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.PerScreen
+import Data.Ratio
+import XMonad.StackSet
+import Text.Regex.Posix
 
-myLayout = 
-    avoidStruts $ 
+myLayout =
+    avoidStruts $
     smartSpacingWithEdge 4 $
     (ifWider 1900 horizontalLayout verticalLayout) ||| Full
   where
     horizontalLayout = Tall 1 (3/100) (1/2)
-    verticalLayout = ThreeColMid 1 (3/100) (1/2)
+    verticalLayout = Mirror (ThreeColMid 1 (3/100) (1/2))
+
+mayavi_root = "Mayavi2 - The 3D data visualizer"
+isModal :: Query Bool
+isModal = isInProperty "_NET_WM_STATE" "_NET_WM_STATE_MODAL"
 
 myManageHook = composeAll [
                manageDocks,
-               className =? "Shutter" --> doFloat,
-               className =? "Gimp" --> doFloat
-               ] 
+               className =? "Gimp" --> doFloat,
+	       className =? "SWT" --> doRectFloat (RationalRect (1%4) (1%4) (1%2) (1%2)),
+	       title =? "FDM Visualizer" --> doShift "3",
+	       isModal --> doFloat
+               ]
 
-myStartupHook = spawnOnce "albert"
+myStartupHook = do
+	setWMName "LG3D" -- Java hack
 
 main = do
-    xmproc <- spawnPipe "/home/johng/.cabal/bin/xmobar"
-    xmonad $ defaultConfig
-        { 
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ desktopConfig
+        {
             modMask = mod4Mask,
             terminal = "termite",
 	    startupHook = myStartupHook,
-            manageHook = myManageHook,
-            layoutHook = myLayout,
+            manageHook = myManageHook <+> manageHook desktopConfig,
+            layoutHook = myLayout ||| layoutHook desktopConfig,
             logHook = dynamicLogWithPP xmobarPP
 			{ ppOutput = hPutStrLn xmproc,
 			  ppTitle = xmobarColor "green" "" . shorten 50
@@ -45,9 +57,10 @@ main = do
 		docksEventHook,
 		handleEventHook defaultConfig ],
             borderWidth = 2
-           } `additionalKeys` [ 
-               ((mod4Mask, xK_b), spawn "chromium"),
+           } `additionalKeys` [
+               ((mod4Mask, xK_c), spawn "chromium"),
+               ((mod4Mask, xK_z), spawn "slock"),
+               ((mod4Mask, xK_s), spawn "slack"),
+               ((mod4Mask, xK_w), renameWorkspace def),
                ((0, xK_Print), spawn "shutter")
              ]
-
-
