a4fcd37b2edee2c24474d425234466a7f26d1914
John Galbraith <jgalbraith@magicleap.com>
2021-05-26 12:50:18 -0700

    Snapshot of ML setup

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -27,98 +27,94 @@
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.config import Click, Drag, Group, Key, Screen, ScratchPad, DropDown
 from libqtile.lazy import lazy
+from libqtile import hook
 
 mod = "mod4"
 
+groups = [
+    Group("VIM"),
+    Group("Visualize"),
+    Group("VS Code"),
+    Group("BG"),
+	ScratchPad("scratchpad", [
+		DropDown("python", "alacritty -e python"),
+		DropDown("vpn", "alacritty -e globalprotect")
+	])
+]
+
 keys = [
     # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down(),
-        desc="Move focus down in stack pane"),
-    Key([mod], "j", lazy.layout.up(),
-        desc="Move focus up in stack pane"),
+    Key([mod], "k", lazy.layout.down(), desc="Move focus down in stack pane"),
+    Key([mod], "j", lazy.layout.up(), desc="Move focus up in stack pane"),
+    Key([mod], "l", lazy.layout.next(), desc="Move focus to next stack"),
+    Key([mod], "m", lazy.layout.maximize(), desc="Maximize window in stack"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Normalize window stack"),
+    Key([mod, "shift"], "space", lazy.layout.flip(), desc="Swap the sizes of MonadTall"),
+    Key([mod], "Return", lazy.layout.swap_main(), desc="Swap the main pane"),
 
     # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down(),
-        desc="Move window down in current stack "),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up(),
-        desc="Move window up in current stack "),
-
-    # Switch window focus to other pane(s) of stack
-    # Key([mod], "space", lazy.layout.next(),
-    #     desc="Switch window focus to other pane(s) of stack"),
+    Key([mod, "control"], "k", lazy.layout.shuffle_down(), desc="Move window down in current stack "),
+    Key([mod, "control"], "j", lazy.layout.shuffle_up(), desc="Move window up in current stack "),
+    Key([mod, "control"], "l", lazy.layout.client_to_next(), desc="Move window to next stack "),
+    Key([mod, "control"], "m", lazy.layout.grow(), desc="Grow the window "),
+    Key([mod, "control"], "n", lazy.layout.shrink(), desc="Shrink the window "),
 
     # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate(),
-        desc="Swap panes of split stack"),
+    Key([mod, "shift"], "space", lazy.layout.flip(), desc="Swap panes of split stack"),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-    Key([mod, "shift"], "Return", lazy.spawn('alacritty'), desc="Launch terminal"),
+    # Key([mod], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
 
     # Toggle between different layouts as defined below
     Key([mod], "space", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating mode"),
 
+    # QTile life cycle
     Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
 
-    # Select group
-    Key([mod], "1", lazy.group['VIM'].toscreen()),
-    Key([mod], "2", lazy.group['WWW'].toscreen()),
-    Key([mod], "3", lazy.group['IDE'].toscreen()),
+    # Emulate XMonad screen switching (Mod + number)
+    *[Key([mod], str(i+1), lazy.group[g.name].toscreen(),
+		    desc="Switch to group {}".format(g.name)
+	 ) for i, g in enumerate(groups)],
+
+    # Emulate XMonad send window to another screen
+    *[Key([mod, "shift"], str(i+1), lazy.window.togroup(g.name),
+		    desc="move focused window to group {}".format(g.name)
+	 ) for i, g in enumerate(groups)],
+
+    # Scratchpad
+    Key([mod], 'F9', lazy.group['scratchpad'].dropdown_toggle('python')),
+    Key([mod], 'F10', lazy.group['scratchpad'].dropdown_toggle('vpn')),
+
+    # Application launches
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod, "shift"], "Return", lazy.spawn('alacritty'), desc="Launch terminal"),
+    Key([mod, "shift"], "c", lazy.spawn('chromium-browser'), desc="Launch Chrome"),
+    Key([mod, "shift"], "m", lazy.spawn('cloudcompare.CloudCompare'), desc="Launch CloudCompare"),
+    Key([mod, "shift"], "g", lazy.spawn('code'), desc="Launch VSCode")
 ]
-
-# groups = [Group(i) for i in "asdfuiop"]
-groups = [
-        Group("VIM"),
-        Group("WWW"),
-        Group("IDE"),
-]
-
-# for i in groups:
-#     keys.extend([
-#         # mod1 + letter of group = switch to group
-#         Key([mod], i.name, lazy.group[i.name].toscreen(),
-#             desc="Switch to group {}".format(i.name)),
-# 
-#         # mod1 + shift + letter of group = switch to & move focused window to group
-#         Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-#             desc="Switch to & move focused window to group {}".format(i.name)),
-#         # Or, use below if you prefer not to switch to that group.
-#         # # mod1 + shift + letter of group = move focused window to group
-#         # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-#         #     desc="move focused window to group {}".format(i.name)),
-#     ])
 
 layouts = [
-    layout.Stack(
-        num_stacks=2, 
-        margin=10, 
-        border_width=2, 
-        border_focus='lightgray', 
+    layout.MonadTall(
+        margin=8,
+        border_width=2,
+        border_focus='lightgray',
         border_normal='darkgray'
-        ),
+    ),
     layout.Max(),
-    # layout.Stack(num_stacks=3),
-    # Try more layouts by unleashing below layouts.
-    # layout.Bsp(),
-    # layout.Columns(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.VerticalTile(
+        margin=8,
+        border_width=2,
+        border_focus='lightgray',
+        border_normal='darkgray'
+    )
 ]
 
 widget_defaults = dict(
@@ -141,27 +137,9 @@ screens = [
                     rounded=False,
                     ),
                 widget.CurrentLayout(),
-                widget.Spacer(length=bar.STRETCH),
-                widget.Clipboard(fmt="{:40}"),
-                widget.Spacer(length=100),
                 widget.WindowName(fmt="{:10}"),
-                widget.Spacer(length=bar.STRETCH),
-                widget.Prompt(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
                 widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(
-                    padding=5,
-                    ),
-                widget.PulseVolume(),
-                widget.TextBox(text="net:"),
-                widget.NetGraph(interface="enp0s31f6", border_width=1, margin_y=0, line_width=2),
-                widget.TextBox(text="cpu:"),
+                widget.Systray(padding=5),
                 widget.CPUGraph(border_width=1, margin_y=0, line_width=2),
                 widget.TextBox(text="mem:"),
                 widget.MemoryGraph(border_width=1, margin_y=0, line_width=2),
@@ -175,7 +153,7 @@ screens = [
         bottom=bar.Bar(
             [
                 widget.CurrentLayout(),
-                widget.GroupBox(),
+                widget.GroupBox()
             ],
             64,
         ),
@@ -199,10 +177,18 @@ bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
+    {'role': 'pop-up'},
+    {'wm_type': 'modal'},
+    {'wm_type': 'dialog'},
+    {'wmclass': 'kazam'}, # Kazam is a video capture tool
+    {'wname': 'kazam'},
     {'wmclass': 'confirm'},
     {'wmclass': 'dialog'},
     {'wmclass': 'download'},
     {'wmclass': 'error'},
+    {'wmclass': 'matplotlib'},
+    {'wmclass': 'Open3D'},
+    {'wmclass': 'python3.8'},
     {'wmclass': 'file_progress'},
     {'wmclass': 'notification'},
     {'wmclass': 'splash'},
@@ -215,7 +201,7 @@ floating_layout = layout.Floating(float_rules=[
     {'wmclass': 'ssh-askpass'},  # ssh-askpass
 ])
 auto_fullscreen = True
-focus_on_window_activation = "smart"
+# focus_on_window_activation = "smart"
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
